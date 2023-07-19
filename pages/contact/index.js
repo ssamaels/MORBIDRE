206edf0b4eaa2ee1dac2fdd7b473c4e22c640b8d
Samael Szep <szepsamael@gmail.com>
2023-07-19 15:21:30 +0200

    contact page styled

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -8,33 +8,70 @@ export default function Contact() {
   return (
     <>
       <Header />
-      <div>
+      <StyledForm>
         <label htmlFor="name" className="name_label">
-          {" "}
           <strong>Name:</strong>
         </label>
-        <input type="text" name="name" id="name" cols="30" rows="5"></input>
+        <StyledInput
+          type="text"
+          name="name"
+          id="name"
+          cols="30"
+          rows="5"
+          required
+        ></StyledInput>
         <label htmlFor="email" className="name_label">
-          {" "}
           <strong>Email:</strong>
         </label>
-        <input type="text" name="email" id="email" cols="30" rows="5"></input>
+        <StyledInput
+          type="text"
+          name="email"
+          id="email"
+          cols="30"
+          rows="5"
+          required
+        ></StyledInput>
         <label htmlFor="Message" className="Message_label">
-          {" "}
           <strong>Message:</strong>
         </label>
-        <textarea
+        <StyledTextArea
           type="text"
           name="notes"
           id="notes"
           cols="30"
-          rows="20"
-        ></textarea>
+          rows="5"
+          required
+        ></StyledTextArea>
         <div className="field">
           <DrawingCanvas />
         </div>
-        <button type="submit">SUBMIT</button>
-      </div>
+        <StyledButton type="submit">SUBMIT</StyledButton>
+      </StyledForm>
     </>
   );
 }
+
+const StyledForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  align-items: center;
+  justify-content: space-around;
+  margin-top: 20px;
+`;
+
+const StyledInput = styled.input`
+  border: 4px solid #1ce598;
+  border-radius: 15px;
+`;
+
+const StyledTextArea = styled.textarea`
+  border: 4px solid #1ce598;
+  border-radius: 15px;
+`;
+
+const StyledButton = styled.button`
+  margin: 10px;
+  background-color: #1ce598;
+  border-radius: 15px;
+`;
